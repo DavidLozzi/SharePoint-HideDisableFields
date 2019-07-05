d21c75dea23641a6cedf63eb92698be51bf4a761
David Lozzi <lozzi.david@bcg.com>
2019-07-05 15:38:47 -0400

    refactored to ES6

--- Lozzi.Fields.js
+++ Lozzi.Fields.js
@@ -2,30 +2,54 @@
 
 /*
 Created by David Lozzi, @davidlozzi, www.davidlozzi.com, 1/10/2014
+Last Updated 7/5/2019
 Requires jQuery, you can include: <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 */
 
-var Lozzi = window.Lozzi || {};
+const fields = {
+    spContext: null,
+    currentUser: null,
+    peopleLoopCount: 0,
+    peoplePickerBorder: "",
+    metadataLoop: 0,
 
-Lozzi.Fields = function () {
-    var spContext;
-    var currentUser;
-    var peoplePickerCell;
+    getFieldType: (theCell) => {
+        theCell.contents().map(() => {
+            if (this.nodeType === 8) {
+                console.log(this);
+                const ftRegx = new RegExp('(FieldType=")([a-zA-Z]*)"','gmi');
+                const vals = ftRegx.exec(this.data);
+                if(vals && vals.length > 2 ) {
+                    console.log(vals[2]);
+                    return vals[2];
+                }
+            }
+        });
+    },
+    
+    disable: (fieldDisplayName) => {
+        const theCell = fields.getCell(fieldDisplayName);
+        const fieldType = fields.getFieldType(theCell);
+        
+        let value = "";
+        let theControls;
 
-    var disable = function (fieldDisplayName) {
-        var theCell = getCell(fieldDisplayName);
-        var value = "";
-        var theControls;
+        switch (fieldType) {
+            case "":
+                break;
+            default:
+        }
+
         if (theCell.find("[class^='sp-peoplepicker']").length > 0) {
-            disablePeoplePicker(theCell);
+            fields.disablePeoplePicker(theCell);
         } else if (theCell.find(".ms-taxonomy-fieldeditor").length > 0) {
-            disableMetadata(theCell);
+            fields.disableMetadata(theCell);
         } else if (theCell.find("[id*='$LookupField']").length > 0) {
-            disableLookupField(theCell);
+            fields.disableLookupField(theCell);
         } else if (theCell.find("input[type='radio']").length > 0) {
-            disableRadioField(theCell);
+            fields.disableRadioField(theCell);
         } else if (theCell.find("span[class='ms-RadioText']>input[type='checkbox']").length > 0) {
-            disableMultiSelectField(theCell);
+            fields.disableMultiSelectField(theCell);
         } else {
             theControls = theCell.find("input,select,textarea,img");
             value = "<span class='readonly'>" + theControls.val() + "<span>";
@@ -33,167 +57,168 @@ Lozzi.Fields = function () {
             theCell.prepend(value);
             theCell.find("div.ms-inputBox").hide();
         }
-    }
+    },
 
-    var peopleLoopCount = 0;
-    var peoplePickerBorder = "";
-    var disablePeoplePicker = function (theCell) {
+    disablePeoplePicker: (theCell) => {
         if ($(".sp-peoplepicker-topLevel", theCell).css("border") != "none") {
             //console.log("disablePeoplePicker")
             $(".sp-peoplepicker-editorInput", theCell).attr("disabled", "true")
-            peoplePickerBorder = $(".sp-peoplepicker-topLevel", theCell).css("border");
+            fields.peoplePickerBorder = $(".sp-peoplepicker-topLevel", theCell).css("border");
             $(".sp-peoplepicker-topLevel", theCell).css("border", "none");
             $(".sp-peoplepicker-delImage", theCell).hide();
 
-            peopleLoopCount++;
+            fields.peopleLoopCount++;
             //hate this loop, not sure how else to handle waiting for the objs 
-            if (peopleLoopCount < 3) {
-                setTimeout(function () { disablePeoplePicker(theCell) }, 350);
+            if (fields.peopleLoopCount < 3) {
+                setTimeout(() => { fields.disablePeoplePicker(theCell) }, 350);
             } else {
-                peopleLoopCount = 0;
+                fields.peopleLoopCount = 0;
             }
         }
-    }
-    var metadataLoop = 0;
-    var disableMetadata = function (theCell) {
-        if ($(".valid-text", theCell).length > 0 || metadataLoop == 5) {
+    },
+
+    disableMetadata: (theCell) => {
+        if ($(".valid-text", theCell).length > 0 || fields.metadataLoop == 5) {
             //console.log("found " + $(".valid-text",theCell).length + " with text " + $(".valid-text",theCell).text());
-            var metaValue = $(".valid-text", theCell).text();
-            var theControls = theCell.find("input,select,textarea,img");
-            var value = "<span class='readonly'>" + metaValue + "<span>";
+            const metaValue = $(".valid-text", theCell).text();
+            const theControls = theCell.find("input,select,textarea,img");
+            const value = "<span class='readonly'>" + metaValue + "<span>";
             theControls.hide();
             theCell.prepend(value);
             theCell.find("div.ms-inputBox").hide();
-            metadataLoop = 0;
+            fields.metadataLoop = 0;
         } else {
-            metadataLoop++;
+            fields.metadataLoop++;
             //yup, hate looping to wait for the DOM to load. Must. Find. Fix.
-            if (metadataLoop <= 5) {
-                setTimeout(function () { disableMetadata(theCell) }, 350);
+            if (fields.metadataLoop <= 5) {
+                setTimeout(() => { fields.disableMetadata(theCell) }, 350);
             }
         }
-    }
+    },
 
-    var disableLookupField = function (theCell) {
-        var theControls = theCell.find("select");
-        var selectedValue = theControls.find("option:selected");
-        var value = "<span class='readonly'>" + selectedValue.text() + "<span>";
+    disableLookupField: (theCell) => {
+        const theControls = theCell.find("select");
+        const selectedValue = theControls.find("option:selected");
+        const value = "<span class='readonly'>" + selectedValue.text() + "<span>";
         theControls.hide();
         theCell.prepend(value);
-    }
+    },
 
-    var disableMultiSelectField = function (theCell) {
-        var theControls = theCell.find("table");
-        var selectedValues = theControls.find("input:checked");
-        var value = "";
-        selectedValues.each(function(index) {
-            var selectedLabel = $(this).siblings("label");
+    disableMultiSelectField: (theCell) => {
+        const theControls = theCell.find("table");
+        const selectedValues = theControls.find("input:checked");
+        let value = "";
+        selectedValues.each(() => {
+            const selectedLabel = $(this).siblings("label");
             value += "<span class='readonly'>" + selectedLabel.text() + "<span><br/>";
         })
         theControls.hide();
         theCell.prepend(value);
-    }
+    },
 
-    var disableRadioField = function (theCell) {
+    disableRadioField: (theCell) => {
         //debugger
-        var theControls = theCell.find("table");
-        var selectedValue = theCell.find("input:checked").val();
+        const theControls = theCell.find("table");
+        const selectedValue = theCell.find("input:checked").val();
         theControls.hide();
         theCell.prepend(selectedValue);
-    }
+    },
 
-    var enable = function (fieldDisplayName) {
-        var theCell = getCell(fieldDisplayName);
+    enable: (fieldDisplayName) => {
+        const theCell = fields.getCell(fieldDisplayName);
         if (theCell.find("[class^='sp-peoplepicker']").length > 0) {
-            enablePeoplePicker(theCell);
+            fields.enablePeoplePicker(theCell);
         } else if (theCell.find(".ms-taxonomy-fieldeditor").length > 0) {
-            enableMetadata(theCell);
+            fields.enableMetadata(theCell);
         } else {
-            var theControls = theCell.find("input,select,textarea,img");
-            var value = theCell.find("span.readonly")
+            const theControls = theCell.find("input,select,textarea,img");
+            const value = theCell.find("span.readonly")
             theControls.show();
             value.remove();
         }
-    }
-    var enablePeoplePicker = function (theCell) {
-        if ($(".sp-peoplepicker-topLevel", theCell).css("border") != peoplePickerBorder) {
+    },
+
+    enablePeoplePicker: (theCell) => {
+        if ($(".sp-peoplepicker-topLevel", theCell).css("border") != fields.peoplePickerBorder) {
             //console.log("enablePeoplePicker")
             $(".sp-peoplepicker-editorInput", theCell).removeAttr("disabled")
-            $(".sp-peoplepicker-topLevel", theCell).css("border", peoplePickerBorder);
+            $(".sp-peoplepicker-topLevel", theCell).css("border", fields.peoplePickerBorder);
             $(".sp-peoplepicker-delImage", theCell).show();
             $("span.readonly", theCell).remove();
-            peopleLoopCount++;
-            if (peopleLoopCount < 7) {
-                setTimeout(function () { enablePeoplePicker(theCell) }, 350);
+            fields.peopleLoopCount++;
+            if (fields.peopleLoopCount < 7) {
+                setTimeout(() => { fields.enablePeoplePicker(theCell) }, 350);
             } else {
-                peopleLoopCount = 0;
+                fields.peopleLoopCount = 0;
             }
         }
-    }
-    var enableMetadata = function (theCell) {
+    },
+
+    enableMetadata: (theCell) => {
         console.log("enableMetadata");
-        if (metadataLoop == 0) {
+        if (fields.metadataLoop == 0) {
             //console.log("found " + $(".valid-text",theCell).length + " with text " + $(".valid-text",theCell).text());
-            var metaValue = $(".valid-text", theCell).text();
-            var theControls = theCell.find("input,select,textarea,img");
+            const theControls = theCell.find("input,select,textarea,img");
             theControls.show();
             $("div.ms-inputBox", theCell).show();
             $("span.readonly", theCell).remove();
         } else {
-            setTimeout(function () { enableMetadata(theCell) }, 350);
+            setTimeout(() => { fields.enableMetadata(theCell) }, 350);
         }
-    }
+    },
 
-    var hide = function (fieldDisplayName) {
-        var theRow = getRow(fieldDisplayName);
+    hide: (fieldDisplayName) => {
+        const theRow = fields.getRow(fieldDisplayName);
         theRow.hide();
-    }
-    var show = function (fieldDisplayName) {
-        var theRow = getRow(fieldDisplayName);
-        theRow.show();
-    }
+    },
 
-    var getRow = function (fieldDisplayName) {
-        var theRow = $("[Title='" + fieldDisplayName + "'], [Title='" + fieldDisplayName + " possible values']").closest("td.ms-formbody").parent();
+    show: (fieldDisplayName) => {
+        const theRow = fields.getRow(fieldDisplayName);
+        theRow.show();
+    },
+
+    getRow: (fieldDisplayName) => {
+        let theRow = $("[Title='" + fieldDisplayName + "'], [Title='" + fieldDisplayName + " possible values']").closest("td.ms-formbody").parent();
         if (theRow.length == 0) {
-            theRow = $(".ms-formlabel").filter(function (index) {
+            theRow = $(".ms-formlabel").filter(() => {
                 return $(this).text().trim() == fieldDisplayName || $(this).text().trim() == fieldDisplayName + " *";
             }).parent();
         }
         return theRow;
-    }
-    var getCell = function (fieldDisplayName) {
-        var theCell = $("[Title='" + fieldDisplayName + "'], [Title='" + fieldDisplayName + " possible values']").closest("td.ms-formbody");
+    },
+
+    getCell: (fieldDisplayName) => {
+        let theCell = $("[Title='" + fieldDisplayName + "'], [Title='" + fieldDisplayName + " possible values']").closest("td.ms-formbody");
         if (theCell.length == 0) {
-            theCell = $(".ms-formlabel").filter(function (index) {
+            theCell = $(".ms-formlabel").filter(() => {
                 return $(this).text().trim() == fieldDisplayName || $(this).text().trim() == fieldDisplayName + " *";
             }).parent().find("td.ms-formbody");
         }
         return theCell;
-    }
+    },
+
     //group names in an array of strings, i.e. ["Group One","Group Two"]
-    var disableWithAllowance = function (fieldName, groups) {
-        disable(fieldName);
+    disableWithAllowance: (fieldName, groups) => {
+        fields.disable(fieldName);
 
-        spContext = new SP.ClientContext.get_current();
-        currentUser = spContext.get_web().get_currentUser();
+        fields.spContext = new SP.ClientContext.get_current();
+        fields.currentUser = fields.spContext.get_web().get_currentUser();
 
-        spContext.load(currentUser);
-        spContext.load(currentUser.get_groups());
-        spContext.executeQueryAsync(function () {
-            getGroupsAndEnable(fieldName, groups)
+        fields.spContext.load(fields.currentUser);
+        fields.spContext.load(fields.currentUser.get_groups());
+        fields.spContext.executeQueryAsync(() => {
+            fields.getGroupsAndEnable(fieldName, groups)
         }, asyncFailed);
+    },
 
-    }
-
-    var getGroupsAndEnable = function (fieldName, groups) {
-        if (currentUser.get_isSiteAdmin()) {
+    getGroupsAndEnable: (fieldName, groups) => {
+        let allowedToEdit = false;
+        if (fields.currentUser.get_isSiteAdmin()) {
             allowedToEdit = true;
         } else {
-            var groupEnum = currentUser.get_groups().getEnumerator();
-            var allowedToEdit = false;
+            const groupEnum = fields.currentUser.get_groups().getEnumerator();
             while (groupEnum.moveNext()) {
-                var group = groupEnum.get_current();
+                const group = groupEnum.get_current();
                 if ($.inArray(group.get_title(), groups) > -1) {
                     allowedToEdit = true;
                     break;
@@ -201,34 +226,32 @@ Lozzi.Fields = function () {
             }
         }
         if (allowedToEdit) {
-            enable(fieldName);
+            fields.enable(fieldName);
         }
-
-    }
+    },
 
     //group names in an array of strings, i.e. ["Group One","Group Two"]
-    var hideWithAllowance = function (fieldName, groups) {
-        hide(fieldName);
+    hideWithAllowance: (fieldName, groups) => {
+        fields.hide(fieldName);
 
-        spContext = new SP.ClientContext.get_current();
-        currentUser = spContext.get_web().get_currentUser();
+        fields.spContext = new SP.ClientContext.get_current();
+        fields.currentUser = fields.spContext.get_web().get_currentUser();
 
-        spContext.load(currentUser);
-        spContext.load(currentUser.get_groups());
-        spContext.executeQueryAsync(function () {
-            getGroupsAndShow(fieldName, groups);
-        }, asyncFailed)
+        fields.spContext.load(fields.currentUser);
+        fields.spContext.load(fields.currentUser.get_groups());
+        fields.spContext.executeQueryAsync(() => {
+            fields.getGroupsAndShow(fieldName, groups);
+        }, asyncFailed);
+    },
 
-    }
-
-    var getGroupsAndShow = function (fieldName, groups) {
-        if (currentUser.get_isSiteAdmin()) {
+    getGroupsAndShow: (fieldName, groups) => {
+        let allowedToEdit = false;
+        if (fields.currentUser.get_isSiteAdmin()) {
             allowedToEdit = true;
         } else {
-            var groupEnum = currentUser.get_groups().getEnumerator();
-            var allowedToEdit = false;
+            const groupEnum = fields.currentUser.get_groups().getEnumerator();
             while (groupEnum.moveNext()) {
-                var group = groupEnum.get_current();
+                const group = groupEnum.get_current();
                 if ($.inArray(group.get_title(), groups) > -1) {
                     allowedToEdit = true;
                     break;
@@ -236,27 +259,21 @@ Lozzi.Fields = function () {
             }
         }
         if (allowedToEdit) {
-            show(fieldName);
+            fields.show(fieldName);
         }
-    }
+    },
 
-    var asyncFailed = function (sender, args) {
+    asyncFailed: (sender, args) => {
         alert('request failed ' + args.get_message() + '\n' + args.get_stackTrace());
-    }
+    },
 
-    var setDefaultValue = function (fieldDisplayName, stringValue) {
+    setDefaultValue: (fieldDisplayName, stringValue) => {
         //TODO flush out the many field type options
-        var theCell = getCell(fieldDisplayName);
-        var selectObj = theCell.find("SELECT");
+        const theCell = fields.getCell(fieldDisplayName);
+        const selectObj = theCell.find("SELECT");
         selectObj.val(stringValue);
-    }
+    },
+};
 
-    return {
-        disable: disable,
-        disableWithAllowance: disableWithAllowance,
-        hide: hide,
-		show: show,
-        hideWithAllowance: hideWithAllowance,
-        setDefaultValue: setDefaultValue
-    }
-}()
+const Lozzi = window.Lozzi || {};
+Lozzi.Fields = fields;
